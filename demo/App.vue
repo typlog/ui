a6fd77d44c3ab2bd380484fa23521ab5b4405f2c
Hsiaoming Yang <me@lepture.com>
2024-10-06 22:47:54 +0900

    chore: update deps

--- demo/App.vue
+++ demo/App.vue
@@ -1,5 +1,11 @@
 <script setup lang="ts">
-import { Button, Checkbox } from '../src'
+import {
+  Button,
+  Checkbox,
+  PopoverRoot,
+  PopoverContent,
+  PopoverTrigger,
+} from '../src'
 </script>
 
 <template>
@@ -10,5 +16,15 @@ import { Button, Checkbox } from '../src'
   <section>
     <h1>Checkbox</h1>
     <Checkbox />
+  </section>
+  <section>
+    <PopoverRoot>
+      <PopoverTrigger as-child>
+        <Button>Show Popover</Button>
+      </PopoverTrigger>
+      <PopoverContent align="start">
+        <p>This is popover content</p>
+      </PopoverContent>
+    </PopoverRoot>
   </section>
 </template>
