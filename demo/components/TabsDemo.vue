2a61a1ef061a5433b27ddb9d1208e4a909ad9f81
Hsiaoming Yang <me@lepture.com>
2025-03-08 16:25:22 +0900

    chore: update deps

--- demo/components/TabsDemo.vue
+++ demo/components/TabsDemo.vue
@@ -10,7 +10,33 @@ import {
 <template>
   <div class="mt-2 flex gap-4">
     <TabsRoot defaultValue="apple">
-      <TabsList>
+      <TabsList size="1">
+        <TabsTrigger value="apple">Apple</TabsTrigger>
+        <TabsTrigger value="banana">Banana</TabsTrigger>
+      </TabsList>
+      <TabsContent value="apple">
+        <p>This is an apple</p>
+      </TabsContent>
+      <TabsContent value="banana">
+        <p>This is an banana</p>
+      </TabsContent>
+    </TabsRoot>
+
+    <TabsRoot defaultValue="apple">
+      <TabsList size="2">
+        <TabsTrigger value="apple">Apple</TabsTrigger>
+        <TabsTrigger value="banana">Banana</TabsTrigger>
+      </TabsList>
+      <TabsContent value="apple">
+        <p>This is an apple</p>
+      </TabsContent>
+      <TabsContent value="banana">
+        <p>This is an banana</p>
+      </TabsContent>
+    </TabsRoot>
+
+    <TabsRoot defaultValue="apple">
+      <TabsList color="gray" high-contrast>
         <TabsTrigger value="apple">Apple</TabsTrigger>
         <TabsTrigger value="banana">Banana</TabsTrigger>
       </TabsList>
