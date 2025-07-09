e5d764f9a803eff5d20a90cbceadfc1ff0fe8edd
Hsiaoming Yang <me@lepture.com>
2025-07-09 22:24:30 +0900

    fix: update build steps

--- demo/main.ts
+++
@@ -1,7 +0,0 @@
-import './base.css'
-import { createApp } from 'vue'
-import App from './App.vue'
-
-App.name = 'Typlog UI'
-const app = createApp(App)
-app.mount('#app')
--- scripts/post-build.mjs
+++ scripts/post-build.mjs
@@ -1,10 +1,22 @@
-import fs from 'node:fs/promises'
+import fs from 'node:fs'
 import path from 'node:path'
 
-const updateImports = async (filePath) => {
-  let content = await fs.readFile(filePath, 'utf-8')
+const updateImports = (filePath) => {
+  let content = fs.readFileSync(filePath, 'utf-8')
   content = content.replace(/#components/g, './components')
-  await fs.writeFile(filePath, content)
+  fs.writeFileSync(filePath, content)
+}
+
+const updatePackageJSON = async () => {
+  let content = fs.readFileSync('package.json', 'utf-8')
+  let pkg = JSON.parse(content)
+  const version = pkg.version
+
+  content = fs.readFileSync('src/package.json', 'utf-8')
+  pkg = JSON.parse(content)
+  pkg.version = version
+  fs.writeFileSync('dist/package.json', JSON.stringify(pkg, null, 2))
 }
 
 updateImports(path.resolve('dist/addons.js'))
+updatePackageJSON()
