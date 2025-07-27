1d436f1801bd2ef11d677b792acfcb4471f3f8bd
Hsiaoming Yang <me@lepture.com>
2025-07-27 11:40:09 +0900

    fix!: remove addon SpinnerIcon

--- src/addons/iconset/index.ts
+++ src/addons/iconset/index.ts
@@ -2,8 +2,3 @@ export {
   default as IconifyCollection,
   type IconifyCollectionProps,
 } from './IconifyCollection.vue'
-
-export {
-  default as SpinnerIcon,
-  type SpinnerIconProps,
-} from './SpinnerIcon.vue'
