15bb65b6fb05282ef50b9af8b4ff6cc926b31ee2
Hsiaoming Yang <me@lepture.com>
2025-08-14 15:33:47 +0900

    docs: update dropdown docs

--- docs/content/components/dropdown.md
+++ docs/content/components/dropdown.md
@@ -8,3 +8,46 @@ radix: https://www.radix-ui.com/themes/docs/components/dropdown-menu
 ---
 
 <Example name="dropdown/Overview.vue" variant="hide" />
+
+## API Reference
+
+### DropdownMenuRoot
+
+Contains all the parts of a dropdown menu.
+
+<PropsTable name="DropdownMenuRoot" />
+
+### DropdownMenuTrigger
+
+Wraps the control that will open the dropdown menu. Usually used with `Button`:
+
+```vue
+<template>
+<DropdownMenuRoot>
+  <DropdownMenuTrigger as-child>
+    <Button>Show dropdown menu</Button>
+  </DropdownMenuTrigger>
+  <DropdownMenuContent>...</DropdownMenuContent>
+</DropdownMenuRoot>
+</template>
+```
+
+<PropsTable name="DropdownMenuTrigger" />
+
+### DropdownMenuContent
+
+The component that pops out when the dropdown menu is open.
+
+<PropsTable name="DropdownMenuContent" />
+
+### DropdownMenuItem
+
+The component that contains the dropdown menu items.
+
+<PropsTable name="DropdownMenuItem" />
+
+## Examples
+
+### Variant
+
+<Example name="dropdown/Variant.vue" />
---
+++ docs/examples/dropdown/Variant.vue
@@ -0,0 +1,58 @@
+<script setup lang="ts">
+import {
+  Button,
+  DropdownMenuRoot,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+} from '#components'
+import CaretDownIcon from '~icons/radix-icons/caret-down'
+</script>
+
+<template>
+  <div class="flex items-center gap-4">
+    <DropdownMenuRoot>
+      <DropdownMenuTrigger>
+        <Button variant="solid">
+          Solid
+          <CaretDownIcon />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent variant="solid">
+        <DropdownMenuItem shortcut="⌘ E">Edit</DropdownMenuItem>
+        <DropdownMenuItem shortcut="⌘ D">Duplicate</DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem shortcut="⌘ N">Archive</DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>Share</DropdownMenuItem>
+        <DropdownMenuItem>Add to favorites</DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem shortcut="⌘ ⌫" color="red">
+          Delete
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenuRoot>
+    <DropdownMenuRoot>
+      <DropdownMenuTrigger>
+        <Button variant="soft">
+          Soft
+          <CaretDownIcon />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent variant="soft">
+        <DropdownMenuItem shortcut="⌘ E">Edit</DropdownMenuItem>
+        <DropdownMenuItem shortcut="⌘ D">Duplicate</DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem shortcut="⌘ N">Archive</DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>Share</DropdownMenuItem>
+        <DropdownMenuItem>Add to favorites</DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem shortcut="⌘ ⌫" color="red">
+          Delete
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenuRoot>
+  </div>
+</template>
